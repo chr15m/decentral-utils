0b5c5b4c40a1e038370cd28fcdab43461f407d03
Chris McCormick <chris@mccormick.cx>
2019-03-29 09:15:37 +0800

    Fixed bugs in sig material construction.

--- index.js
+++ index.js
@@ -41,10 +41,12 @@ if (typeof(TextEncoder) == "object") {
 // e.g. 4:salt6:foobar3:seqi4e1:v12:Hello world!
 function make_sig_check(struct) {
   var seq = isNaN(struct.seq) ? 1 : Math.max(Math.floor(struct.seq), 1);
-  var header = "4:salt" + utf8_to_u8(struct.salt).length + ":" + struct.salt + "3:seq" + "i" + seq + "e" + "1:v" + struct.v.length;
-  var check = new Uint8Array(header.length + struct.v.length);
+  var salt = struct.salt ? struct.salt.substr(0, 64) : null;
+  var header = (struct.salt ? ("4:salt" + utf8_to_u8(struct.salt).length + ":" + struct.salt) : "") + "3:seq" + "i" + seq + "e" + "1:v" + struct.v.length;
+  var header_length = utf8_to_u8(header).length;
+  var check = new Uint8Array(header_length + struct.v.length);
   check.set(utf8_to_u8(header));
-  check.set(struct.v, header.length)
+  check.set(struct.v, header_length)
   return check;
 }
 
--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var nacl = require('tweetnacl');
 
 
 test('Test conversion functions', function (t) {
-  t.plan(7);
+  t.plan(8);
 
   t.deepEqual(d.hex_to_u8("deadbeefc0de"), new Uint8Array([222, 173, 190, 239, 192, 222]), "Check hex_to_u8");
   t.deepEqual(d.hex_to_u8("0xdeadbeefc0de"), new Uint8Array([222, 173, 190, 239, 192, 222]), "Check hex_to_u8 with 0x");
@@ -24,5 +24,11 @@ test('Test conversion functions', function (t) {
     "salt": "中英字典",
     "seq": 5,
     "v": d.utf8_to_u8("One two three foobar."),
-  })), "4:salt12:中英字典3:seqOne two three foobar.", "Check make_sig_check");
+  })), "4:salt12:中英字典3:seqi5e1:v21One two three foobar.", "Check make_sig_check");
+
+  t.equal(d.u8_to_utf8(d.make_sig_check({
+    "seq": 7,
+    "v": d.utf8_to_u8("One two three goober."),
+  })), "3:seqi7e1:v21One two three goober.", "Check make_sig_check");
+
 });
