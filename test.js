7dafa64d2b711067ed5fda348a571aa4919718ca
Chris McCormick <chris@mccormick.cx>
2019-03-29 09:23:39 +0800

    BEP0044 sig test vectors.

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var nacl = require('tweetnacl');
 
 
 test('Test conversion functions', function (t) {
-  t.plan(8);
+  t.plan(10);
 
   t.deepEqual(d.hex_to_u8("deadbeefc0de"), new Uint8Array([222, 173, 190, 239, 192, 222]), "Check hex_to_u8");
   t.deepEqual(d.hex_to_u8("0xdeadbeefc0de"), new Uint8Array([222, 173, 190, 239, 192, 222]), "Check hex_to_u8 with 0x");
@@ -30,5 +30,15 @@ test('Test conversion functions', function (t) {
     "seq": 7,
     "v": d.utf8_to_u8("One two three goober."),
   })), "3:seqi7e1:v21One two three goober.", "Check make_sig_check");
+
+  // bep 0044 test vector 1
+  t.equal(d.u8_to_utf8(d.make_sig_check({
+    "v": d.utf8_to_u8("Hello World!"),
+  })), "3:seqi1e1:v12:Hello World!", "Check make_sig_check (bep0044 check)");
+
+  t.equal(d.u8_to_utf8(d.make_sig_check({
+    "salt": "foobar",
+    "v": d.utf8_to_u8("Hello World!"),
+  })), "4:salt6:foobar3:seqi1e1:v12:Hello World!", "Check make_sig_check (bep0044 check)");
 
 });
